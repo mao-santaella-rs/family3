678f999dc471e74115e37c010bc2d364ee88be8a
Mauricio Santaella Ochoa <mao8a87@gmail.com>
2018-06-24 13:41:08 -0500

    base-mao primer commit

---
+++ source/app/views/Home.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+
+	#home
+		vcomponent
+
+</template>
+
+<script>
+import vcomponent from '../components/Component.vue'
+// SI SE VAN A COMENTAR LOS IMPORTS SE TIENEN QUE PONER DEBAJO DE LOS QUE NO ESTAN COMENTADOS
+// import db from '../firebase/init'
+// import axios from 'axios'
+
+export default {
+	name: 'home',
+
+	components: {
+		vcomponent,
+	},
+
+	data(){
+		return{
+		}
+	},
+
+	methods:{
+	},
+	
+	computed:{
+	},
+
+	created(){
+	},
+
+	mounted(){
+	}
+
+}
+</script>
+
+<style lang="sass">
+
+</style>
